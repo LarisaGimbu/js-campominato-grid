c90eb5ca3e8db8acf262b1a535dfe180cec6a8a1
LarisaGimbu <gimbularisa@gmail.com>
2021-11-02 16:50:37 +0100

    square-clicked

--- js/script.js
+++ js/script.js
@@ -16,12 +16,21 @@ btnPlay.addEventListener('click', function(){
   console.log(squareNumber);
 
   for(let i = 0; i < squareNumber; i++){
-    //creo l'elemento square e lo aggiungo al main-container
+    //creo l'elemento square e lo aggiungo al main-container con dentro un numero da q a squareNumber
     const sq = createSquare(container);
+    sq.innerHTML = i + 1;
+
+    sq.addEventListener('click', function(){
+      this.classList.add('clicked');
+    })
   };
 });
 
-
+/**
+ * generatore di square
+ * @param {HTMLDivElement} target 
+ * @returns 
+ */
 function createSquare(target){
   const sq = document.createElement('div');
   let classLevel = '';
@@ -31,8 +40,6 @@ function createSquare(target){
   if(level.value === '1'){
     sq.classList.add('easy');
     sq.classList.remove('hard', 'crazy')
-    
-
   }else if(level.value === '2'){
     sq.classList.add('hard');
     sq.classList.remove('easy', 'crazy')
